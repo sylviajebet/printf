070803811b00509958b6b24b1f3bbfc8567a4e1f
sylviajebet <sylviajebett@gmail.com>
2022-05-23 01:39:59 +0300

    get_flag.c modified

--- get_flag.c
+++ get_flag.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * get_flag - turns on flags if _printf finds
+ * get_flag - Turns on flags if _printf finds
  * a flag modifier in the format string
  * @s: character that holds the flag specifier
  * @f: pointer to the struct flags in which we turn the flags on
  *
- * Return: 1 if a flag has been turned on, 0 otherwise
+ * Return: 1 if a flag has been turned on, otherwise 0
  */
+
 int get_flag(char s, flags_t *f)
 {
 	int i = 0;
